596127cdb087965abb4bc917013e3829ee21944e
Ataaanuu <atanukarmakar657@gmail.com>
2024-12-27 03:11:37 +0530

    boolean
    somthing new

--- hello.java
+++ hello.java
@@ -1,6 +1,16 @@
-public class hello {
+public class hello 
+{
     public static void main(String[] args) 
-    {
-        System.out.println("Hello, World!");
+   {
+    int x = 50;
+    int y = 40;
+    boolean result = 50 < 40;
+    
+    
+   
+       
+       
+        System.out.println(result);
     }
 }
+dfgrg
